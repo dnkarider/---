0b7118908241bfa7b03bf7f7845f6c6f3d9e4452
dnkarider <prodanila1234@mail.ru>
2024-02-16 17:06:17 +0300

    dntknownmbr

---
+++ StreamAPI/1task/src/StreamMain.java
@@ -0,0 +1,12 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class StreamMain {
+public void operations(List<Integer> intList){
+    intList.stream()
+            .filter(x -> x > 0)
+            .filter(x -> x % 2 == 0)
+            .sorted(Comparator.naturalOrder())
+            .forEach(x -> System.out.print(x + " "));
+}
+}
