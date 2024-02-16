72bfcd331910d7edd9d9eff07f591e00544628b7
dnkarider <prodanila1234@mail.ru>
2024-02-16 23:30:11 +0300

    kicker

--- StreamAPI/1task/src/Main.java
+++ StreamAPI/1task/src/Main.java
@@ -1,11 +1,23 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-        System.out.print("Вывод через Stream: \n");
-        StreamMain.operations(intList);
-        System.out.println("\nВывод через for + if:");
-        ForIfMain.forIfMain(intList);
+        List<Integer> newList = new ArrayList<>();
+        System.out.print("Вывод через ForIf: \n");
+        for(int number : intList){
+            if(number > 0 && number % 2 == 0){
+                newList.add(number);
+            }
+        }
+        newList.sort(Comparator.naturalOrder());
+        for (int result : newList){
+            System.out.print(result + " ");
+        }
+        System.out.print("\nВывод через Stream: \n");
+        StreamMain.operations();
     }
 }
--- StreamAPI/1task/src/StreamMain.java
+++ StreamAPI/1task/src/StreamMain.java
@@ -1,12 +1,14 @@
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 public class StreamMain {
-public static void operations(List<Integer> intList){
-    intList.stream()
-            .filter(x -> x > 0)
-            .filter(x -> x % 2 == 0)
-            .sorted(Comparator.naturalOrder())
-            .forEach(x -> System.out.print(x + " "));
-}
+    static List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+    public static void operations(){
+        intList.stream()
+                .filter(x -> x > 0)
+                .filter(x -> x % 2 == 0)
+                .sorted(Comparator.naturalOrder())
+                .forEach(x -> System.out.print(x + " "));
+    }
 }
